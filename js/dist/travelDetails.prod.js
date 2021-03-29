ea28f5e905496d09e903736631be2826bf37bbfb
narendraselot <nselot24@gmail.com>
2021-03-30 01:13:31 +0530

    Final Changes Done By nandu

--- js/dist/travelDetails.prod.js
+++ js/dist/travelDetails.prod.js
@@ -1,1 +1,1 @@
-"use strict";$(function(){});
+"use strict";$(function(){var a=window.location.href,r=new URL(a).searchParams.get("loc");$("#aTagYatra").attr("href",CurrentLocationURLs_Yatra[r]),$("#aTagTrivago").attr("href",CurrentLocationURLs_Trivago[r]),$("#aTagOyo").attr("href",CurrentLocationURLs_OYO[r])});
--- js/travelDetails.js
+++ js/travelDetails.js
@@ -1,3 +1,9 @@
 $(function () {
-   
+    var url_string = window.location.href; //window.location.href
+    var url = new URL(url_string);
+    var loc = url.searchParams.get("loc");
+
+    $("#aTagYatra").attr("href", CurrentLocationURLs_Yatra[loc]);
+    $("#aTagTrivago").attr("href", CurrentLocationURLs_Trivago[loc]);
+    $("#aTagOyo").attr("href", CurrentLocationURLs_OYO[loc]);
 });
