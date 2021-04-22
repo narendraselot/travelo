7ec887c3dabab5a7b6981ba20eb01bff23a9050e
narendraselot <nselot24@gmail.com>
2021-04-23 01:09:10 +0530

    Emergency Services Change

--- js/indexMain.js
+++ js/indexMain.js
@@ -164,7 +164,28 @@ $(function () {
         else
             toast("Oops, Please select a valid Location", "info");
     });
+    $("#txtSearchTableEmergencyNumbers").on("input", function () {
+        searchTableEmergency();
+    });
 });
+function searchTableEmergency() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("txtSearchTableEmergencyNumbers");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tblEmergencyNumbers");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText || td.innerHTML;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
 
 function setMobileMenu() {
     if (window.matchMedia("(max-width: 767px)").matches) {
